df999ebe16f5bc933eba497421cc52ee518bd847
Thivakaran1610 <133901717+Thivakaran1610@users.noreply.github.com>
2024-06-13 00:16:18 +0530

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var flipbook = document.querySelector('.flipbook');
 
-    var totalPages = 5; // Change this to the number of your screenshots
+    var totalPages = 10; // Change this to the number of your screenshots
 
     for (var i = 1; i <= totalPages; i++) {
         var page = document.createElement('div');
         page.classList.add('page');
-        page.style.backgroundImage = 'url(screenshots/page' + i + '.jpg)';
+        page.style.backgroundImage = 'url(screenshots/page' + i + '.png)';
         flipbook.appendChild(page);
     }
 
@@ -14,5 +14,13 @@ document.addEventListener("DOMContentLoaded", function() {
         width: '100%',
         height: '100%',
         autoCenter: true
+    });
+
+    document.querySelector('.prev').addEventListener('click', function() {
+        $('.flipbook').turn('previous');
+    });
+
+    document.querySelector('.next').addEventListener('click', function() {
+        $('.flipbook').turn('next');
     });
 });
